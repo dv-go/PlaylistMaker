c5ffe9431127a985ba7a06d3655a30b1043501ed
Artur A <artur.axmetov@yandex.ru>
2024-10-29 03:31:38 +0300

    First commit

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button1 = findViewById<Button>(R.id.button_1)
+        val button2 = findViewById<Button>(R.id.button_2)
+        val button3 = findViewById<Button>(R.id.button_3)
+
+        val button1ClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val displayIntent = Intent(this@MainActivity, Search::class.java)
+                startActivity(displayIntent)
+            }
+        }
+
+        val button2ClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val displayIntent = Intent(this@MainActivity, Media::class.java)
+                startActivity(displayIntent)
+            }
+        }
+
+        button1.setOnClickListener(button1ClickListener)
+        button2.setOnClickListener(button2ClickListener)
+
+        button3.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
         }
     }
 }
