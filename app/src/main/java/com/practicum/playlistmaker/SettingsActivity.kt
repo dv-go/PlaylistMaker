a24417ab402e85622e781158a7bde4cc8d825162
Artur A <artur.axmetov@yandex.ru>
2024-11-14 21:56:39 +0300

    Коммит 1 Спринт 9

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -12,8 +14,58 @@ class SettingsActivity : AppCompatActivity() {
 
         val backButton = findViewById<TextView>(R.id.backButton)
         backButton.setOnClickListener {
-            val displayIntent = Intent(this@SettingsActivity, MainActivity::class.java)
-            startActivity(displayIntent)
+            finish()
         }
+
+        val shareButton = findViewById<TextView>(R.id.settings_item_2)
+        shareButton.setOnClickListener {
+            shareLink()
+        }
+
+        val supportButton = findViewById<TextView>(R.id.settings_item_3)
+        supportButton.setOnClickListener {
+            openEmailClient()
+        }
+
+        val agreementButton = findViewById<TextView>(R.id.settings_item_4)
+        agreementButton.setOnClickListener {
+            openUserAgreement()
+        }
+    }
+
+    private fun shareLink() {
+        val link = getString(R.string.user_agreement_url)
+        val shareIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, link)
+            type = "text/plain"
+        }
+        val chooserIntent = Intent.createChooser(shareIntent, getString(R.string.share_button_text))
+        startActivity(chooserIntent)
+    }
+
+    private fun openEmailClient() {
+        val email = getString(R.string.support_email)
+        val subject = getString(R.string.email_subject)
+        val body = getString(R.string.email_body)
+
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "message/rfc822"
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, body)
+        }
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(Intent.createChooser(intent, getString(R.string.support_button_text)))
+        } else {
+            Toast.makeText(this, getString(R.string.no_email_client), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun openUserAgreement() {
+        val url = getString(R.string.user_agreement_url)
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
     }
 }
