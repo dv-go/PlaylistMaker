dc13e54dd9f9d8b2990342d8a06bf7a58b930e1f
Artur A <artur.axmetov@yandex.ru>
2024-12-02 17:52:38 +0300

    Спринт 10 Коммит 1

---
+++ app/src/main/java/com/practicum/playlistmaker/TracksViewHolder.kt
@@ -0,0 +1,35 @@
+package com.practicum.playlistmaker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+
+class TracksViewHolder(parent: ViewGroup): RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+) {
+    private val trackName: TextView = itemView.findViewById(R.id.trackName)
+    private val artistName: TextView = itemView.findViewById(R.id.artistName)
+    private val trackTime: TextView = itemView.findViewById(R.id.trackTime)
+    private val artworkImage: ImageView = itemView.findViewById(R.id.artworkImage)
+
+    fun bind(model: Track){
+        trackName.text = model.trackName
+        artistName.text = model.artistName
+        trackTime.text = model.trackTime
+        Glide.with(itemView.context)
+            .load(model.artworkUrl100)
+            .placeholder(R.drawable.placeholder)
+            .apply(
+                RequestOptions()
+                    .transform(RoundedCorners(2))
+                    .fitCenter()
+            )
+            .into(artworkImage)
+    }
+}
